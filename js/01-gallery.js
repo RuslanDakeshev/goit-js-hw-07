e9dabcf21aff8ceb194d7f03ac8ee11ca68c7394
RuslanDakeshev <bil08@outlook.com>
2022-09-17 15:51:09 +0200

    Summary

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.innerHTML = createGalleryMarkup(galleryItems);
+
+galleryEl.addEventListener('click', onGalleryClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') return;
+
+  const instance = basicLightbox.create(
+    `
+    <img src='${event.target.dataset.source}' width="800" height="600">
+`,
+    {
+      onShow: () => document.addEventListener('keydown', onEscapeBtnPress),
+      onClose: () => document.removeEventListener('keydown', onEscapeBtnPress),
+    }
+  );
+
+  instance.show();
+
+  function onEscapeBtnPress(event) {
+    if (event.key === 'Escape') {
+      instance.close();
+    }
+  }
+}
